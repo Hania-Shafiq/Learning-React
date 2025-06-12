2a60f99d902c9f7a9f0d6556026d7f44088dba41
Hania Shafiq <shafiqhania00@gmail.com>
2025-06-12 11:04:14 +0500

    created 1st project

---
+++ twist-text/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Navbar from './components/Navbar.js';
+
+function App() {
+  return (
+
+<Navbar title="TwistText" aboutText="About Twist Text"/>
+
+  );
+  
+}
+
+export default App;
